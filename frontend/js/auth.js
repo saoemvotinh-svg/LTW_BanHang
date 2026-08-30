4e59dbf22eb7188a77fdc12bf426fc4ce6153215
Tran Minh Hung <hungtm1944@ut.edu.vn>
2026-08-30 16:29:22 +0700

    login layout

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -2,10 +2,113 @@ const loginheader = document.querySelector(".login-header h1")
 const Eregister = document.querySelector(".register")
 const passwordBox = document.querySelector(".password-box");
 const submitbutton = document.querySelector(".login-button");
+var username = document.querySelector('input[name="username"]');
 var password = document.querySelector('input[name="password"]');
 var button = passwordBox.querySelector("button");
+const form = document.querySelector(".login-form");
+const error = document.querySelector(".error");
 
 var isRegister = false;
+
+async function login() {
+
+    const data = {
+        username: username.value,
+        password: password.value
+    };
+
+    if (!username.value.trim() || !password.value.trim()) {
+        error.innerText = "Vui lòng nhập đầy đủ trường!"
+        error.style.display = "block";
+
+        return;
+    }
+    error.style.display = "none";
+
+    try {
+
+        const response = await fetch("#", {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        console.log(result);
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+}
+
+async function register() {
+
+    const confirmPassword = document.querySelector(".confirm-password");
+    const passwordError = document.querySelector(".password-error");
+
+    const data = {
+        username: username.value,
+        password: password.value
+    };
+
+    if (!username.value.trim() || !password.value.trim()) {
+        error.innerText = "Vui lòng nhập đầy đủ trường!"
+        error.style.display = "block";
+
+        return;
+    }
+        error.style.display = "none";
+    
+    if (data.password !== confirmPassword.value) {
+
+        passwordError.style.display = "block";
+        return;
+    } else {
+
+        passwordError.style.display = "none";
+
+    }
+
+    try {
+
+        const response = await fetch("#", {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        console.log(result);
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+}
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (isRegister) {
+        register();
+    } else {
+        login();
+    }
+});
+
 function changemode() {
     if (!isRegister) {
         Eregister.innerHTML = `
@@ -16,11 +119,18 @@ function changemode() {
           </a>
         `
         passwordBox.insertAdjacentHTML("afterend", `
-        <input type="password" class="confirm-password" placeholder="Nhập lại mật khẩu">
+        <div class="confirm-password-box">
+
+            <input type="password" class="confirm-password" placeholder="Nhập lại mật khẩu">
+
+            <div class="password-error">
+              Mật khẩu không khớp
+            </div>
+
+          </div>
         `);
         submitbutton.innerText = "Đăng ký";
         loginheader.innerText = "Đăng ký";
-        
         var forgotPassword = document.querySelector(".forgot-password");
 
         if (forgotPassword) {
@@ -35,7 +145,7 @@ function changemode() {
             Đăng ký
           </a>
         `
-        var confirmPassword = document.querySelector(".confirm-password");
+        var confirmPassword = document.querySelector(".confirm-password-box");
 
         if (confirmPassword) {
             confirmPassword.remove();
