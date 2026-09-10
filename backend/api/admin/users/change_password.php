18037ba5dc041c2a03026721c9f108f9f131372c
Tran Minh Hung <hungtm1944@ut.edu.vn>
2026-09-10 18:03:12 +0700

    fix admin backend

--- backend/api/admin/users/change_password.php
+++ backend/api/admin/users/change_password.php
@@ -1,15 +1,6 @@
 <?php
 // backend/api/admin/users/change_password.php
 
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
 
 require_once __DIR__ . '/../auth_check.php';
 
